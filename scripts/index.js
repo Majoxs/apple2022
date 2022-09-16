7061b7318227c15002b7d65d9c8fc68244c3b6ad
Majoxs <majoxs@gmail.com>
2022-09-16 17:18:12 +0500

    Added cuntry selection and currency converter functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,16 @@
 import Swiper from '../lib/swiper-bundle.esm.browser.min.js';
 
+// SimpleBar
+
+new SimpleBar(document.querySelector('.country__list'), {
+  classNames: {
+    scrollbar: 'country__scrollbar',
+    track: 'country__track'
+  }
+});
+
+// Slider
+
 new Swiper('.goods__block', {
   slidesPerView: 1,
   spaceBetween: 20,
@@ -22,10 +33,97 @@ new Swiper('.goods__block', {
   navigation: {
     prevEl: '.goods__arrow--prev',
     nextEl: '.goods__arrow--next'
+  },
+  preventClicks: true,
+  a11y: false,
+});
+
+// Modal
+
+const productMore = document.querySelectorAll('.product__more');
+const modal = document.querySelector('.modal');
+const formPlaceholder = document.querySelectorAll('.form__placeholder');
+const formInput = document.querySelectorAll('.form__input');
+
+productMore.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modal.classList.add('modal--open');
+  })
+});
+
+modal.addEventListener('click', e => {
+  if (e.target === modal) {
+    modal.classList.remove('modal--open');
   }
 });
 
-// Плавный скролл
+formInput.forEach((input, i) => {
+  input.addEventListener('focus', () => {
+    formPlaceholder[i].classList.add('form__placeholder--active')
+  });
+
+  input.addEventListener('blur', () => {
+    if (input.value === '') {
+      formPlaceholder[i].classList.remove('form__placeholder--active')
+    }
+  });
+});
+
+// Currency
+
+const dataCurrency = {};
+
+const formatCurrency = (value, currency) => {
+  return new Intl.NumberFormat('EU', {
+    style: 'currency',
+    currency,
+    maximumFractionDigits: 2,
+  }).format(value);
+};
+
+const showPrice = (currency = 'USD') => {
+  const priceElems = document.querySelectorAll('[data-price]');
+
+  priceElems.forEach(elem => {
+    elem.textContent = formatCurrency(elem.dataset.price * dataCurrency[currency], currency);
+  });
+};
+
+const myHeaders = new Headers();
+myHeaders.append("apikey", "zZu3nnzCgY2YaPiMeYPZDIw5sUI6n3g9");
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/fixer/latest?&base=USD", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    Object.assign(dataCurrency, result.rates)
+    showPrice();
+  })
+  .catch(error => console.log('error', error));
+
+
+// Choices
+
+const countryBtn = document.querySelector('.country__btn');
+const countryWrapper = document.querySelector('.country__wrapper');
+
+countryBtn.addEventListener('click', () => {
+  countryWrapper.classList.toggle('country__wrapper--open');
+});
+
+countryWrapper.addEventListener('click', ({target}) => {
+  if (target.classList.contains('country__choise')) {
+    countryWrapper.classList.remove('country__wrapper--open');
+    showPrice(target.dataset.currency);
+  }
+});
+
+// Smoth scroll
 
 const smothScroll = (trigger) => {
   const SPEED = 0.3;
