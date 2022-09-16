db4800f2f36807672e75d0a882c2039833a73a2c
Majoxs <majoxs@gmail.com>
2022-09-16 09:42:15 +0500

    Added smothScroll

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,43 @@ new Swiper('.goods__block', {
     nextEl: '.goods__arrow--next'
   }
 });
+
+// Плавный скролл
+
+const smothScroll = (trigger) => {
+  const SPEED = 0.3;
+  const scrolled = e => {
+    e.preventDefault();
+    const target = e.target;
+
+    if (target.matches('[href^="#"]')) {
+      let start = 0;
+      const pageY = window.pageYOffset;
+      const hash = target.getAttribute('href');
+
+      if (hash === '#') return;
+
+      const elem = document.querySelector(hash);
+      const coordinateElem = elem.getBoundingClientRect().top;
+      const allDistance = pageY + coordinateElem;
+      const scroll = time => {
+        if (!start) start = time;
+        const progress = time - start;
+        const r = (coordinateElem < 0 ?
+          Math.max(pageY - progress / SPEED, allDistance) :
+          Math.min(pageY + progress / SPEED, allDistance));
+
+        window.scrollTo(0, r);
+
+        const scrolling = coordinateElem < 0 ?
+          r > allDistance :
+          r < allDistance;
+        if (scrolling) requestAnimationFrame(scroll);
+      }
+      requestAnimationFrame(scroll)
+    }
+  }
+  trigger.addEventListener('click', scrolled);
+}
+smothScroll(document.querySelector('.header__navigation'))
+smothScroll(document.querySelector('.footer__navigation'))
